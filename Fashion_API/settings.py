76389144d8ce876ed769496c0948dc81c0c89c27
sumondebnath <debnathsumon1708@gmail.com>
2024-06-09 06:09:43 +0600

    fashion api csrf token added

--- Fashion_API/settings.py
+++ Fashion_API/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://fashion-api-g1d6.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
